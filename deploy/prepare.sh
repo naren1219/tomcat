af8dc55861991dcafaacb3154720ca6d0425a544
narendar <narendarn@9acts.com>
2017-12-01 15:32:01 +0000

    updated files

--- deploy/prepare.sh
+++ deploy/prepare.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 BASEDIR=$(dirname $0)
@@ -9,7 +8,7 @@ IMAGE=749833379596.dkr.ecr.us-east-1.amazonaws.com/genospace/tomcat7
 
 jar -xvf VN_sample.war
 
-eval $(aws ecr get-login --region us-west-2 --no-include-email)
+eval $(aws ecr get-login --region us-east-1 --no-include-email)
 
 # Build the app image:
 sudo docker build --force-rm=true -t $IMAGE:$VERSION .
